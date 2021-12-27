370b1efe3fbc332fc7beef72050ffe52b357fcf5
thisisntnathan <74419098+thisisntnathan@users.noreply.github.com>
2021-12-27 18:08:48 -0500

    Add ax- and eq- meCyclohexane files
    Added files for axial and equatorial methylcyclohexane calculations

---
+++ eqMeCyclohexane/g16run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -p chemq                        # submit to partition: chemq
+#SBATCH -J eqMeCyhex                    # job name
+#SBATCH -o %x_oe                        # name of stdout/stderr file
+#SBATCH -N 1                            # total number of nodes requested
+#SBATCH --ntasks-per-node=16            # total number of tasks requested
+#SBATCH --mincpus=16                    # assign at least 16 CPUs from each node
+#SBATCH --mem=0                         # allocate all of the node's available memory
+#SBATCH -t 240:00:00                    # run time (hhh:mm:ss)
+#SBATCH --mail-type=ALL                 # send emails at job START/END/FAIL
+#SBATCH --mail-user=<yourNetID>@cornell.edu
+
+g16root=/software                       # the g16.profile file defines the g16
+. $g16root/g16/bsd/g16.profile          # defaults; ask admin for its location
+export GAUSS_SCRDIR=/scratch
+export GAUSS_CDEF='0-15'                # see https://gaussian.com/link0/ for
+export GAUSS_MDEF='28GB'                # environment variable definitions
+export GAUSS_YDEF='eqMeCyhex.chk'
+
+$g16root/g16/g16 eqMeCyhex.com eqMeCyhex.log
